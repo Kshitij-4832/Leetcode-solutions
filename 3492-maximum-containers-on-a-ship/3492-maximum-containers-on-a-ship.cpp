050106cfec70930ac6702a4aade3ec3819b17101
Kshitij-4832 <kshitijgupta4832@gmail.com>
2026-06-22 16:13:04 +0530

    Time: 0 ms (100%), Space: 8.4 MB (64.08%) - LeetHub

---
+++ 3492-maximum-containers-on-a-ship/3492-maximum-containers-on-a-ship.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxContainers(int n, int w, int maxWeight) {
+        /*int count = 0;
+        for(int i=0;i<(n*n);i++){
+            if(maxWeight>=w){
+                maxWeight -=w; 
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        return count;*/
+
+        return min(n*n,maxWeight/w);
+    }
+};

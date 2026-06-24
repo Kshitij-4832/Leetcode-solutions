807f0be5d2a4c58d8e87cbc631178c64f9291c8b
Kshitij <kshitijgupta4832@gmail.com>
2026-06-24 16:28:41 +0530

    Time: 0 ms (100%), Space: 17.6 MB (53.45%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,39 +1,51 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-    // Find the starting and ending index for the repeated number was the most challenging part via Binary Search.
-    vector<int> vec = {0,0};
-    int size = nums.size();
-    int start = 0, end = size - 1;
-    while (start <= end)
-    {
-        int mid = (start + end) / 2;
-        if (nums[mid] == target)
-        {   
-            while(start<=end){
-                if(nums[start]==nums[end]){
-                    vec[0] = start;
-                    vec[1] = end;
-                    return vec;
-                }
-                else if(nums[start]<target){
-                    start++;
-                }
-                else{
-                    end--;
-                }
+    int search_left(int target,vector<int>& nums,bool checkleft){
+        int start = 0,end =nums.size()-1,ans = 0;
+        while(start<=end){
+            int mid =  (start+end) /2;
+            if(nums[mid]==target){
+                ans =  mid;  
+                end = mid-1;       
+                checkleft=true;
+            }
+            else if(target>nums[mid]){
+                start = mid+1;
+            }
+            else{
+                end =  mid-1;
             }
         }
-        if (target >= nums[mid])
-        {
-            start = mid + 1;
+        if(checkleft){
+            return ans;
         }
-        if(target<=nums[mid])
-        {
-            end = mid - 1;
-        }
+        return -1;
     }
-    vec[0] = -1,vec[1] = -1;
-    return vec;
+     int search_right(int target,vector<int>& nums,bool checkright){
+        int start = 0,end =nums.size()-1,ans = 0;
+        while(start<=end){
+            int mid =  (start+end) /2;
+            if(nums[mid]==target){
+                ans =  mid;  
+                start = mid+1; 
+                checkright = true;      
+            }
+            else if(target>nums[mid]){
+                start = mid+1;
+            }
+            else{
+                end =  mid-1;
+            }
+        }
+        if(checkright){
+            return ans;
+        }
+        return -1;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int>index = {-1,-1};
+        index[0] = search_left(target,nums,false);
+        index[1] = search_right(target,nums,false);
+        return index;
     }
 };

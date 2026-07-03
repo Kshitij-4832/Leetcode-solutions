0a3304061b3640409dde545e08487838a822fadd
Kshitij <kshitijgupta4832@gmail.com>
2026-07-03 20:59:40 +0530

    Time: 2 ms (45.74%), Space: 7.7 MB (88.52%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -4,9 +4,6 @@ public:
         if(c==1){
         return true;
         }
-        if(c==INT_MAX){
-            return false;
-        }
         long long int a = 0,b=sqrt(c);
         while(a<=b){
             if(a*a+b*b==c){

6915395873f181a8b186e1afc117745344588739
Kshitij <kshitijgupta4832@gmail.com>
2026-07-19 20:35:08 +0530

    Time: 0 ms (100%), Space: 8.1 MB (50.9%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -4,7 +4,7 @@ public:
         //Use Exponent of prime where prime number must be 5 and it's higher powers.
         int ans = 0,p = 5;
         while(p<=n){
-            ans = ans + floor((double)n/p);
+            ans = ans + (n/p);
             p = p*5;
         }   
         return ans;

fde74cf316ffe6640e989cf569fae681bb5f4e51
Kshitij <kshitijgupta4832@gmail.com>
2026-07-19 20:14:32 +0530

    Time: 0 ms (100%), Space: 8.3 MB (16.65%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        //Use Exponent of prime where prime number must be 5 and it's higher powers.
+        int ans = 0,p = 5;
+        while(p<=n){
+            ans = ans + floor((double)n/p);
+            p = p*5;
+        }   
+        return ans;
+    }
+};

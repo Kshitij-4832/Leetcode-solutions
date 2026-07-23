173401592335b51a5790628771ee04ad48e5949f
Kshitij <kshitijgupta4832@gmail.com>
2026-07-23 13:47:25 +0530

    Time: 0 ms (100%), Space: 15.8 MB (43.12%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,29 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        //C++ STL
-        next_permutation(nums.begin(),nums.end());
+        int size = nums.size();
+        for (int i = size - 1; i >= 0; i--) {
+            if (i != 0) {
+                if (nums[i] > nums[i - 1]) {
+                    int breakpoint = nums[i - 1];
+                    int index = 0;
+                    for (int j = i; j < size; j++) {
+                        if (nums[j] > breakpoint) {
+                            index = j;
+                        }
+                    }
+                    swap(nums[i - 1], nums[index]);
+                    sort(nums.begin() + i, nums.end());
+                    break;
+                }
+            } else {
+                int start = 0, end = size - 1;
+                while (start < end) {
+                    swap(nums[start], nums[end]);
+                    start++;
+                    end--;
+                }
+            }
+        }
     }
 };

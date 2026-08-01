435a2d4997bd3b0b8691e3f286c9bc78595e7b7d
Kshitij <kshitijgupta4832@gmail.com>
2026-08-01 12:51:31 +0530

    Time: 18 ms (39.13%), Space: 31.8 MB (60.18%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -2,32 +2,19 @@ class Solution {
 public:
     int minSteps(string s, string t) {
         int s1 = s.length(), t1 = t.length();
-        unordered_map<char, int> mps;
-        unordered_map<char, int> mpt;
-        for (int i = 0; i < s1; i++) {
-            mps[s[i]]++;
-        }
-        for (int i = 0; i < t1; i++) {
-            mpt[t[i]]++;
+        vector<int>vec1(26,0);
+        vector<int>vec2(26,0);
+        for(int i = 0;i<s1;i++){
+            vec1[(int)s[i]-97]++;
         }
 
-        int ans1 = 0,ans2=0;
-        for(auto p:mps){
-            if(mps.find(p.first)!=mpt.end()){
-                ans1 = ans1+abs(p.second-mpt[p.first]);
-            }
-            else{
-                ans1 = ans1+p.second;
-            }
+        for(int i = 0;i<t1;i++){
+            vec2[(int)t[i]-97]++;
         }
-        for(auto p:mpt){
-            if(mpt.find(p.first)!=mps.end()){
-                ans2 = ans2+abs(p.second-mps[p.first]);
-            }
-            else{
-                ans2 = ans2+p.second;
-            }
+        int ans = 0;
+        for(int i = 0;i<26;i++){
+            ans += abs(vec1[i]-vec2[i]);
         }
-        return ans2;
+        return ans;
     }
 };

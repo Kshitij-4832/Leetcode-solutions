2d83589b6742550edb67698a7431b0e33604d58c
Kshitij <kshitijgupta4832@gmail.com>
2026-08-21 12:09:15 +0530

    Time: 2 ms (36.7%), Space: 10 MB (37.73%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    void F(vector<int>& nums, int index, vector<int>& arr,
-           vector<vector<int>>& res) {
-        if (index >= nums.size()) {
-            res.push_back(arr);
-            return;
+    void Subsets(vector<int>& nums,int index,vector<vector<int>> & matrix,vector<int>& arr){
+        if(index>=nums.size()){
+            matrix.push_back(arr);
+            return ;
         }
         arr.push_back(nums[index]);
-        F(nums, index + 1, arr, res);
+        Subsets(nums,index+1,matrix,arr);//for taking the element
         arr.pop_back();
-        F(nums, index + 1, arr, res);
+        Subsets(nums,index+1,matrix,arr);//for not taking the element
+
+        return;
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> arr;
-        int index = 0;
-        F(nums, 0, arr, res);
-        return res;
+        vector<vector<int>> matrix;
+        vector<int>arr;
+        Subsets(nums,0,matrix,arr);
+        return matrix;
     }
 };

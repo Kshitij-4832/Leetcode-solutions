3b14e99f86ee2c58908d179830f50c0e9cf69114
Kshitij <kshitijgupta4832@gmail.com>
2026-08-22 12:01:32 +0530

    Time: 2 ms (57.97%), Space: 14 MB (77.16%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    void F(int index,int target,vector<int>& candidates,vector<int>& ds,vector<vector<int>>& matrix){
+    void F(int index,int target,vector<int>& candidates,vector<int>& path,vector<vector<int>>& combination_sets){
         if(index==candidates.size()){
             if(target==0){
-                matrix.push_back(ds);
+                combination_sets.push_back(path);
             }
             return ;
         }
         if(candidates[index]<=target){
-            ds.push_back(candidates[index]);
-            F(index,target-candidates[index],candidates,ds,matrix);
-            ds.pop_back();
+            path.push_back(candidates[index]);
+            F(index,target-candidates[index],candidates,path,combination_sets);
+            path.pop_back();
         }
-        F(index+1,target,candidates,ds,matrix);
+        F(index+1,target,candidates,path,combination_sets);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int>ds;
-        vector<vector<int>> matrix;
-        F(0,target,candidates,ds,matrix);
-        return matrix;   
+        vector<int>path;
+        vector<vector<int>> combination_sets;
+        F(0,target,candidates,path,combination_sets);
+        return combination_sets;   
     }
 };

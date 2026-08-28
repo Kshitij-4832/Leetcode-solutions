db93a2cbb57963267df9a5e199a8fc897c67a2b9
Kshitij <kshitijgupta4832@gmail.com>
2026-08-28 16:46:54 +0530

    Time: 0 ms (100%), Space: 31.4 MB (46.17%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int size =  nums.size();
-        int start =0,end=size-1;
-        while(start<=end){
-            int mid =  start+(end-start)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(target>nums[mid]){
-                start = mid+1;
-            }
-            else{
-                end =  mid-1;
-            }
+    int Func(vector<int>& nums,int left,int right,int target){
+        //Recursive Binary Search
+        if(left>right){
+            return -1;
         }
-        return -1;
+        int mid = left+(right-left)/2;
+        if(nums[mid]==target)
+        {
+            return mid;
+        }
+        else if(nums[mid]>target){
+            return Func(nums,left,mid-1,target);
+        }
+        else{
+            return Func(nums,mid+1,right,target);
+        }
+    }
+    int search(vector<int>& nums, int target) {
+        int res = Func(nums,0,nums.size()-1,target);
+        return res;
     }
 };

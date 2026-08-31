434ba4506558cbff788765c3cf8c82a44aa2de5d
Kshitij <kshitijgupta4832@gmail.com>
2026-08-31 12:28:25 +0530

    Time: 16 ms (5.16%), Space: 128.6 MB (7.49%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -12,31 +12,31 @@ class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
         vector<int> ans = {-1, -1};
-        vector<int> criticalpoints;
         if (head->next->next == NULL) {
             return ans;
         }
-        int itr = 2;
-        ListNode* curr = head->next;
         ListNode* prev = head;
+        ListNode* curr = head->next;
         ListNode* Next = head->next->next;
-        while (curr->next != NULL) {
-            if (curr->val > prev->val && curr->val > Next->val ||
-                curr->val < prev->val && curr->val < Next->val) {
-                criticalpoints.push_back(itr);
+        int itr = 2;
+        vector<int> cps;
+        while (Next != NULL) {
+            int x = curr->val, y = prev->val, z = Next->val;
+            if (x > y && x > z || x < y && x < z) {
+                cps.push_back(itr);
             }
             itr++;
-            curr = curr->next;
             prev = prev->next;
+            curr = curr->next;
             Next = Next->next;
         }
-        sort(criticalpoints.begin(), criticalpoints.end());
-        int size = criticalpoints.size();
+        int size = cps.size();
         if (size >= 2) {
+            sort(cps.begin(),cps.end());
             ans[0] = INT_MAX;
-            ans[1] = criticalpoints[size - 1] - criticalpoints[0];
-            for (int i = 0; i < size - 1; i++) {
-                ans[0] = min(ans[0], criticalpoints[i + 1] - criticalpoints[i]);
+            ans[1] = cps[size - 1] - cps[0];
+            for(int i = 1;i<size;i++){
+                ans[0] = min(ans[0],abs(cps[i]-cps[i-1]));
             }
         }
         return ans;

e6caf44dcb3aead26640cc7e76b00af5ec631009
Kshitij Gupta <kshitijgupta4832@gmail.com>
2026-09-02 18:49:30 +0530

    Time: 0 ms (100%), Space: 8.4 MB (87.43%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x == 0) {
-            return 0;
-        }
-        if (x == 1 || x == 2) {
-            return 1;
-        }
-
-        int start = 1, end = x;
-        long long int mid = -1;
-
-        while (start <= end) {
-            mid = start+(end-start) / 2;
-            if (mid * mid > x) {
-                end = mid - 1;
-            } else if (mid * mid == x) {
+        int left = 1,right = x;
+        int ans = 0;
+        while(left<=right){
+            long long int mid = left+(right-left)/2;
+            if(mid*mid==x){
                 return mid;
-            } else {
-                start = mid + 1;
+            }
+            if(mid*mid>x){
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
+                ans = mid;
             }
         }
-        return end;
+        return ans;
     }
 };

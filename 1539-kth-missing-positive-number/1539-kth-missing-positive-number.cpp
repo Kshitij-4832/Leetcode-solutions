95a0ca9263f07b6834b2ea43357aafb2fed06fb4
Kshitij Gupta <kshitijgupta4832@gmail.com>
2026-09-08 18:24:38 +0530

    Time: 0 ms (100%), Space: 13.4 MB (29.24%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -4,14 +4,18 @@ public:
         int left = 0, right = arr.size() - 1;
         while (left <= right) {
             int mid = (left + right) / 2;
-            int missing  =  arr[mid]-(mid+1);
+            int missing = arr[mid] - (mid + 1);
             if(missing<k){
-                left  = mid+1;
+                left = mid+1;
             }
             else{
                 right  = mid-1;
             }
         }
+        //   ans = arr[right]-more
+        //more = k-missing
+        //missing = arr[right] - right -1;
+        //therefore  ans = right + 1 +k or ans = left+k
         return left+k;
     }
 };

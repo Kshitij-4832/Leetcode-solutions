05c77e768969e25e2138a2ea4f4bf87b3258cb3c
Kshitij Gupta <kshitijgupta4832@gmail.com>
2026-09-13 20:12:01 +0530

    Time: 39 ms (72.45%), Space: 84.9 MB (34.56%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -6,17 +6,15 @@ public:
         int left = 0, right = 0, maxlen = 0;
         while (right < size) {
             mpp[fruits[right]]++;
-            if(mpp.size()>2){
-                while(mpp.size()>2){
-                    mpp[fruits[left]]--;
-                    if(mpp[fruits[left]]==0){
-                        mpp.erase(fruits[left]);
-                    }
-                    left++;
+            if (mpp.size() > 2) {
+                mpp[fruits[left]]--;
+                if (mpp[fruits[left]] == 0) {
+                    mpp.erase(fruits[left]);
                 }
+                left++;
             }
-            else{
-                maxlen = max(maxlen,right-left+1);
+            if (mpp.size() <= 2) {
+                maxlen = max(maxlen, right - left + 1);
             }
             right++;
         }

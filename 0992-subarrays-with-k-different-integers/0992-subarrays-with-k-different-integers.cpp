66671e8e4032b6209ad438618eac9945c142ff7d
Kshitij Gupta <kshitijgupta4832@gmail.com>
2026-09-21 22:04:36 +0530

    Time: 91 ms (31.72%), Space: 56.1 MB (57.45%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
+    int count_subarray_less_than_k(vector<int>&nums,int k){
+        int left = 0,right = 0,size = nums.size(),count = 0;
+        unordered_map<int,int>mpp;
+        while(right<size){
+            mpp[nums[right]]++;
+            while(mpp.size()>k){
+                mpp[nums[left]]--;
+                if(mpp[nums[left]]==0){
+                    mpp.erase(nums[left]);
+                }
+                left++;
+            }
+            if(mpp.size()<=k){
+                count = count+(right-left+1);
+            }
+            right++;
+        }
+        return count;
+    }
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        int left = 0, right = 0, size = nums.size();
-        int c1 = 0, c2 = 0;
-        unordered_map<int, int> mpp;
-        while (right < size) {
-            mpp[nums[right]]++;
-            while (mpp.size() > k) {
-                mpp[nums[left]]--;
-                if(mpp[nums[left]]==0){
-                    mpp.erase(nums[left]);
-                }
-                left++;
-            }
-            if (mpp.size() <= k) {
-                c1 = c1 + (right - left + 1);
-            }
-            right++;
-        }
-        left = 0,right = 0;
-        mpp.clear();
-        while (right < size) {
-            mpp[nums[right]]++;
-            while (mpp.size() > k-1) {
-                mpp[nums[left]]--;
-                if(mpp[nums[left]]==0){
-                    mpp.erase(nums[left]);
-                }
-                left++;
-            }
-            if (mpp.size() <= k-1) {
-                c2 = c2 + (right - left + 1);
-            }
-            right++;
-        }
-        return abs(c2 - c1);
+        int c1 = count_subarray_less_than_k(nums,k);
+        k--;
+        int c2 = count_subarray_less_than_k(nums,k);
+        return c1-c2;
     }
 };

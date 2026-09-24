508b9a84bc73addc09eb86a8a226d1652079a2a2
Kshitij Gupta <kshitijgupta4832@gmail.com>
2026-09-24 14:27:06 +0530

    Time: 0 ms (100%), Space: 10.7 MB (87.72%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        int size = s.length();
+        unordered_map<char,int> mpp;
+        for (int i = 0; i < size; i++) {
+            mpp[s[i]]++;
+        }
+
+        string ans = "";
+        while (mpp.size() != 0) {
+            int Max = 0;
+            char ch;
+            for (auto itr : mpp) {
+                if (Max < itr.second) {
+                    Max = itr.second;
+                    ch = itr.first;
+                }
+            }
+            while(Max--){
+                ans.push_back(ch);
+            }
+            mpp.erase(ch);
+        }
+        return ans;
+    }
+};
